b29b4b05d3ebc02f26a9933f83a1c55cf0880deb
alexandrumanta13 <alexandru.manta@hotmail.com>
2020-11-29 18:44:23 +0200

    chore: header + slider homepage

--- src/app/pages/home/slider/slider.component.ts
+++ src/app/pages/home/slider/slider.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-slider',
@@ -9,9 +10,35 @@ export class SliderComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 0,
+    nav: true,
+    animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
     
-   
+    smartSpeed: 1000,
+
+    navText: ['<span class="icon-Arrow-Left"></span>', '<span class="icon-Arrow-Right"></span>'],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      800: {
+        items: 1
+      },
+      1024: {
+        items: 1
+      }
+    }
+  }
+
+  ngOnInit(): void {
+
+
   }
 
 }
